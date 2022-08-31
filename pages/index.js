b43468e3deb702321bbd0ad4074fe2b6f556c6bb
MulamanO2 <95545958+MulamanO2@users.noreply.github.com>
2022-08-31 07:34:10 -0700

    Update and rename index.tsx to index.js

---
+++ pages/index.js
@@ -0,0 +1,76 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = require("next/router");
+var Home_module_css_1 = require("../styles/Home.module.css");
+var Home = function () {
+    var router = (0, router_1.useRouter)();
+    return className = { styles: Home_module_css_1.default, : .container } >
+        { /* Top Section */}
+        < h1;
+    className = { styles: Home_module_css_1.default, : .h1 } > Stake;
+    a;
+    Exclusive;
+    Bored;
+    Kitty & Earn;
+    BK22;
+    Tokens < /h1>
+        < b;
+    className = { styles: Home_module_css_1.default, : .h1 } > Free;
+    Limited;
+    Amount;
+    then;
+    will;
+    have;
+    to;
+    buy in MATIC < /b>
+        < div;
+    className = { styles: Home_module_css_1.default, : .nftBoxGrid };
+    role = "button";
+    onClick = {}();
+};
+router.push("/mint");
+    >
+        className;
+{
+    Home_module_css_1.default.optionSelectBox;
+}
+role = "button";
+onClick = {}();
+router.push("/stake");
+    >
+        src;
+{
+    "/icons/token.webp";
+}
+alt = "drop" /  >
+    className;
+{
+    Home_module_css_1.default.selectBoxTitle;
+}
+ > Stake;
+Your;
+Bored;
+Kitty < /h2>
+    < p;
+className = { styles: Home_module_css_1.default, : .selectBoxDescription } >
+    We;
+Will;
+watch;
+Your;
+Kitties;
+keep;
+them;
+safe;
+and;
+sound.They;
+'ll be with other kitties {" "}, while they are safe and sound you';
+ll;
+earn < b > BK22;
+Tokens < /b> while you're eating pizza or playing games.
+    < /p>
+    < /div>
+    < /div>
+    < /div>;
+;
+;
+exports.default = Home;
